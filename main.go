12318923ba6a832262c2994da2ed62b129ce66d8
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-17 22:53:04 +0700

    feat: implement file reading functionality in main application

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+	defer f.Close()
+
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+
+	for {
+		b := make([]byte, 8, 8)
+		n, err := f.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(b[:n])
+		fmt.Printf("read: %s\n", str)
+	}
 }
