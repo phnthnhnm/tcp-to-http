f4c205f8034c4e532860737a8cdb1ad67607c9af
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-18 12:14:42 +0700

    feat: refactor file reading to use channels for improved line handling

--- main.go
+++ main.go
@@ -16,32 +16,51 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
 
-	currentLineContents := ""
-	for {
-		buffer := make([]byte, 8, 8)
-		n, err := f.Read(buffer)
-		if err != nil {
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
+	linesChan := getLinesChannel(f)
+
+	for line := range linesChan {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer func(f io.ReadCloser) {
+			err := f.Close()
+			if err != nil {
+				fmt.Printf("error closing file: %s\n", err.Error())
+			}
+		}(f)
+		defer close(lines)
+
+		currentLineContents := ""
+		for {
+			b := make([]byte, 8, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(b[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
 				currentLineContents = ""
 			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			currentLineContents += parts[len(parts)-1]
 		}
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts)-1]
-	}
+	}()
+
+	return lines
 }
