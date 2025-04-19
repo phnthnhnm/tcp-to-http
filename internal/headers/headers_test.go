4f08f0376891f931c4135fb400510a2b2fe6095b
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-19 19:50:46 +0700

    test(headers): rename test function and add case for duplicate header keys

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestHeaderParse(t *testing.T) {
+func TestHeadersParse(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
 	data := []byte("Host: localhost:42069\r\n\r\n")
@@ -63,5 +63,15 @@ func TestHeaderParse(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Same header key
+	headers = map[string]string{"host": "localhost:8000"}
+	data = []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:8000, localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
 	assert.False(t, done)
 }
